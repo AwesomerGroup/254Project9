d8f0fa8e818f50909ba32011966348e4660d4ac2
Kathy Saad <ksaad@csu.fullerton.edu>
2014-04-30 11:36:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "function.h"
-<<<<<<< HEAD
 
-=======
->>>>>>> 3057cb119d7554ca2f26a32d8e09d1d730662c37
 using namespace std;
 
 int main(){
@@ -11,13 +8,9 @@ int main(){
 	int a=0;
 	x = 2 + 2;
 	cout << x << endl;
-<<<<<<< HEAD
 	cout << "OUTPUT TEXT";
-	
-=======
-	cout << "OUTPUT TEXT" ;
 	a=multiply(x);
 	cout <<a <<endl;
->>>>>>> 3057cb119d7554ca2f26a32d8e09d1d730662c37
+
 	return 0;
 }
