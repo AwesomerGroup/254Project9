3057cb119d7554ca2f26a32d8e09d1d730662c37
Noeli Murrietta <noeliluv27@csu.fullerton.edu>
2014-04-30 11:12:40 -0700

    new main and function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-
+#include "function.h"
 using namespace std;
 
 int main(){
-	int x;
-	
+	int x=0;
+	int a=0;
 	x = 2 + 2;
 	cout << x << endl;
 	cout << "OUTPUT TEXT" ;
-	
+	a=multiply(x);
+	cout <<a <<endl;
 	return 0;
 }
